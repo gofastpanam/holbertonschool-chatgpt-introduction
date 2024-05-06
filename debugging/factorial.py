6a395d89c3c9da5ab80640bfadae0e3668ff004e
root <root@BENJ>
2024-05-06 15:11:21 +0200

    push factorial function

---
+++ debugging/factorial.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import sys
+
+if len(sys.argv) != 2:
+    print("Usage: python3 script.py <number>")
+    sys.exit(1)
+
+def factorial(n):
+    result = 1
+    while n > 1:
+        result *= n
+        n = n - 1
+    return result
+
+f = factorial(int(sys.argv[1]))
+print(f)
